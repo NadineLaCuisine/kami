eb352183a92b6029828d9270a559622875f675d5
cmarchet <camille.marchet@inria.fr>
2016-02-11 19:42:47 +0100

    parser in utils

---
+++ utils.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "utils.h"
+
+using namespace std;
+
+
+void split(const string& s, char delim,vector<string>& v) {
+    auto i = 0;
+    auto pos = s.find(delim);
+    while (pos != string::npos) {
+      v.push_back(s.substr(i, pos-i));
+      i = ++pos;
+      pos = s.find(delim, pos);
+
+      if (pos == string::npos)
+         v.push_back(s.substr(i, s.length()));
+    }
+}
---
+++ utils.h
@@ -0,0 +1,3 @@
+using namespace std;
+
+void split(const string& s, char delim,vector<string>& v);
