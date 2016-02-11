47ab9edde38b19c1bcf87e6f6759550dc58381ec
cmarchet <camille.marchet@inria.fr>
2016-02-11 18:05:53 +0100

    1st commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+using namespace std;
+
+
+
+int main(int argc, char ** argv){
+	if (argc < 3){
+		cout << "command line: ./kMILL reads.fasta k" << endl;
+	} else {
+		string fileName = argv[1];
+		uint k = stoi(argv[2]);
+		ifstream readStructFile(fileName);
+        ofstream out("out.fa");
+		
+		//~ for (uint i(0); i<sequencesVec.size(); ++i){
+			//~ if (not sequencesVec[i].sequence.empty()){
+				//~ out<<sequencesVec[i].sequence << endl;
+			//~ }
+		//~ }
+	}
+	return 0;
+}
